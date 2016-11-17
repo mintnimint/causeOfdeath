75fedfa0ccc32d3c656cae6b803a5da295d552a2
Chonlada Kerdkom <s5070027@kmitl.ac.th>
2016-11-17 19:38:22 +0700

    กราฟอัตราปี2552

---
+++ rate2009.py
@@ -0,0 +1,8 @@
+"""Plot total rates of cause death year 2009"""
+from numpy import arange
+from matplotlib import pyplot
+def graph_rate_2009():
+    """show total rates of cause death graph from 2009"""
+    label = ["Cancer", "Accident", "Blood", "Heart Disease", "Lung Disease", "Nephritis",\
+             "Liver Disease", "Commit Suicide", "Diabetes", "Tuberculosis"]
+    Value = [88.3, 55.6, 24.7, 29, 22.9, 20.8, 13.5, 10.5, 11.1, 7.2]
