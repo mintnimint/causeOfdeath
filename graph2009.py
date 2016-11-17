e4c316321517b165536464da1dec674cf64e4179
Chonlada Kerdkom <s5070027@kmitl.ac.th>
2016-11-17 19:23:24 +0700

    กราฟจำนวนปี 2552

---
+++ graph2009.py
@@ -0,0 +1,8 @@
+"""Plot total of cause death year 2009"""
+from numpy import arange
+from matplotlib import pyplot
+def graph_2009():
+    """show total of cause death graph from 2009"""
+    label = ["Cancer", "Accident", "Blood", "Heart Disease", "Lung Disease", "Nephritis",\
+             "Liver Disease", "Commit Suicide", "Diabetes", "Tuberculosis"]
+    Value = [56058, 35304, 15648, 18375, 14542, 13191, 8562, 6642, 7019, 4568]
