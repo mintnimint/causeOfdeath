0a88a82c45f7b0f0a9e1b80caf336bfe7af5fd52
Chonlada Kerdkom <s5070027@kmitl.ac.th>
2016-11-27 00:56:51 +0700

    change file name

--- graph2553.py
+++
@@ -1,8 +0,0 @@
-"""Plot total of cause death year 2010"""
-from numpy import arange
-from matplotlib import pyplot
-def graph_2553():
-    """show total of cause death graph from 2010"""
-    label = ["Cancer", "Accident", "Blood", "Heart Disease", "Lung Disease", "Nephritis",\
-             "Liver Disease", "Commit Suicide", "Diabetes", "Tuberculosis"]
-    Value = [58076, 32861, 20018, 18399, 16369, 13763, 8788, 7062, 6855, 4467]
