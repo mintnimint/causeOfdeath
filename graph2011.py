e6654976b2123269e2b3a1a7767715c08989f3fe
Manassanann <simlimyim@gmail.com>
2016-11-27 13:04:53 +0700

    change value

--- graph2011.py
+++ graph2011.py
@@ -10,9 +10,8 @@ def graph2011():
     sizes = [61082, 33868, 22947, 20130, 16884, 14753, 9009, 6814, 7625, 4784]
     colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'yellow', 'red', 'white', 'blue', 'magenta', 'cyan']
     explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-
     plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-        autopct='%1.1f%%', shadow=True, startangle=90)
+        autopct='%1.1f%%', shadow=True, startangle=140)
 
     # Set aspect ratio to be equal so that pie is drawn as a circle.
     plt.axis('equal')
