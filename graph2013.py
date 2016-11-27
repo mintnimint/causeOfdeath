1fac3473f1158c87ca0a6df4cf8a46585040688d
Manassanann <simlimyim@gmail.com>
2016-11-27 19:56:15 +0700

    change value

--- graph2013.py
+++ graph2013.py
@@ -8,9 +8,9 @@ def graph2013():
     labels = "Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease", "Lung Disease", "Nephritis", "Liver Disease", "Commit Suicide", "Diabetes", "Tuberculosis"
     sizes = [67692, 32422, 28408, 24597, 21676, 15162, 10747, 6986, 9703, 5496]
     colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'yellow', 'red', 'white', 'blue', 'magenta', 'cyan']
-    explode = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-        autopct='%1.1f%%', shadow=True, startangle=90)
+        autopct='%1.1f%%', shadow=True, startangle=140)
 
     # Set aspect ratio to be equal so that pie is drawn as a circle.
     plt.axis('equal')
