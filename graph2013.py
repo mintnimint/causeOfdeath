67030b1384aa2a36a8d70832a508b24bbde5d228
Manassanann <simlimyim@gmail.com>
2016-11-27 19:55:35 +0700

    add fn and doctoring

--- graph2013.py
+++ graph2013.py
@@ -3,7 +3,8 @@
 import matplotlib.pyplot as plt
 
 # The slices will be ordered and plotted counter-clockwise.
-
+def graph2013():
+    """show graph that Plot total of cause death year 2013"""
     labels = "Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease", "Lung Disease", "Nephritis", "Liver Disease", "Commit Suicide", "Diabetes", "Tuberculosis"
     sizes = [67692, 32422, 28408, 24597, 21676, 15162, 10747, 6986, 9703, 5496]
     colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'yellow', 'red', 'white', 'blue', 'magenta', 'cyan']
@@ -14,3 +15,4 @@ import matplotlib.pyplot as plt
     # Set aspect ratio to be equal so that pie is drawn as a circle.
     plt.axis('equal')
     plt.show()
+graph2013()
