cae1de55827354ac501d49d74318d4d14ecb8eb5
Manassanann <simlimyim@gmail.com>
2016-11-27 13:06:33 +0700

    add doctoring

--- graph2011.py
+++ graph2011.py
@@ -1,11 +1,11 @@
-"""Plot Graph"""
+"""Plot Graph by matplotlib"""
 
 import matplotlib.pyplot as plt
 
 # The slices will be ordered and plotted counter-clockwise.
 
 def graph2011():
-    """"""
+    """Show graph that Plot total of cause death year 2011"""
     labels = "Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease", "Lung Disease", "Nephritis", "Liver Disease", "Commit Suicide", "Diabetes", "Tuberculosis"
     sizes = [61082, 33868, 22947, 20130, 16884, 14753, 9009, 6814, 7625, 4784]
     colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'yellow', 'red', 'white', 'blue', 'magenta', 'cyan']
