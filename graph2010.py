5f2f1b68c0ca8ded65fec9e9c0fa35413da7d6ae
Manassanann <simlimyim@gmail.com>
2016-11-27 20:52:03 +0700

    change colors

--- graph2010.py
+++ graph2010.py
@@ -8,7 +8,7 @@ def graph2010():
 
     labels = "Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease", "Lung Disease", "Nephritis", "Liver Disease", "Commit Suicide", "Diabetes", "Tuberculosis"
     sizes = [58076, 32861, 20018, 18399, 16369, 13763, 8788, 7062, 6855, 4467]
-    colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'yellow', 'red', 'white', 'blue', 'magenta', 'cyan']
+    colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'lightgrey', 'tomato', 'seagreen', 'royalblue', 'sienna', 'tan']
     explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     plt.pie(sizes, explode=explode, labels=labels, colors=colors,
         autopct='%1.1f%%', shadow=True, startangle=140)
