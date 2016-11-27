59c832f0a8ceb8e21db9bf371cb672f9e30d476e
Manassanann <simlimyim@gmail.com>
2016-11-27 22:28:24 +0700

    change colors

--- graph2011.py
+++ graph2011.py
@@ -8,7 +8,7 @@ def graph2011():
     """Show graph that Plot total of cause death year 2011"""
     labels = "Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease", "Lung Disease", "Nephritis", "Liver Disease", "Commit Suicide", "Diabetes", "Tuberculosis"
     sizes = [61082, 33868, 22947, 20130, 16884, 14753, 9009, 6814, 7625, 4784]
-    colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'yellow', 'red', 'white', 'blue', 'magenta', 'cyan']
+    colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'lightgrey', 'tomato', 'seagreen', 'royalblue', 'sienna', 'tan']
     explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     plt.pie(sizes, explode=explode, labels=labels, colors=colors,
         autopct='%1.1f%%', shadow=True, startangle=140)
