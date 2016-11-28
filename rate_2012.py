1183b7db642102f1c56f72cb248e70772349b1ec
SukanlayaL <s6070140@kmitl.ac.th>
2016-11-28 21:37:42 +0700

    Update rate_2012.py

--- rate_2012.py
+++ rate_2012.py
@@ -1,3 +1,4 @@
+"""Plot rate graph in year 2012"""
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,9 +12,19 @@ rects1 = ax.bar(ind + width, rate, width, color="r")
 
 ax.set_ylabel("Rates")
 ax.set_title("Cause of Death in Year 2012 (Rates)")
-ax.set_xticks(ind + width)
+ax.set_xticks(ind + width+0.2)
 ax.set_xticklabels(("Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease",\
                      "Lung Disease", "Nephritis", "Liver Disease", "Commit Suicide", "Diabetes",\
-                     "Tuberculosis"))
+                     "Tuberculosis"),rotation="vertical")
 
+def autolabel(rects):
+    # attach some text labels
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%.1f' % height,
+                ha='center', va='bottom')
+
+autolabel(rects1)
 plt.show()
+
