567c55f26f52346a6958f03f161db20ee646b7c6
Manassanann <simlimyim@gmail.com>
2016-11-29 19:05:21 +0700

    clean code

--- graph2009.py
+++ graph2009.py
@@ -3,16 +3,16 @@
 import matplotlib.pyplot as plt
 
 # The slices will be ordered and plotted counter-clockwise.
+def graph2009():
+    """show graph that Plot total of cause death year 2009"""
+    labels = "Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease", "Lung Disease", "Nephritis", "Liver Disease", "Commit Suicide", "Diabetes", "Tuberculosis"
+    sizes = [56058, 35304, 15648, 18375, 14542, 13191, 8562, 6642, 7019, 4568]
+    colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'lightgrey', 'tomato', 'seagreen', 'royalblue', 'sienna', 'tan']
+    explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=140)
 
-labels = "Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease", "Lung Disease", "Nephritis", "Liver Disease", "Commit Suicide", "Diabetes", "Tuberculosis"
-sizes = [56058, 35304, 15648, 18375, 14542, 13191, 8562, 6642, 7019, 4568]
-colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'lightgrey', 'tomato', 'seagreen', 'royalblue', 'sienna', 'tan']
-explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-
-plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-        autopct='%1.1f%%', shadow=True, startangle=90)
-
-# Set aspect ratio to be equal so that pie is drawn as a circle.
-plt.axis('equal')
-
-plt.show()
+    # Set aspect ratio to be equal so that pie is drawn as a circle.
+    plt.axis('equal')
+    plt.show()
+graph2009()
