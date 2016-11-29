019021c23bf50d9b19a6453a7a304e7ad52e11df
Manassanann <simlimyim@gmail.com>
2016-11-29 19:28:40 +0700

    adjust file

--- rate_2011.py
+++ rate_2011.py
@@ -2,28 +2,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 10
-rate = (95.2, 52.8, 35.8, 31.4, 26.3, 23, 14, 10.6, 11.9, 7.5)
-ind = np.arange(n)
-width = 0.35
+def rate_2011():
+    """show rate graph cause of death in year 2011"""
+    n = 10
+    rate = (95.2, 52.8, 35.8, 31.4, 26.3, 23, 14, 10.6, 11.9, 7.5)
+    ind = np.arange(n)
+    width = 0.35
 
-fig, ax = plt.subplots()
-rects1 = ax.bar(ind + width, rate, width, color="r")
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(ind + width, rate, width, color="r")
 
-ax.set_ylabel("Rates")
-ax.set_title("Cause of Death in Year 2011 (Rates)")
-ax.set_xticks(ind + width + 0.2)
-ax.set_xticklabels(("Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease",\
+    ax.set_ylabel("Rates")
+    ax.set_title("Cause of Death in Year 2011 (Rates)")
+    ax.set_xticks(ind + width + 0.2)
+    ax.set_xticklabels(("Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease",\
                      "Lung Disease", "Nephritis", "Liver Disease", "Commit Suicide", "Diabetes",\
                      "Tuberculosis"), rotation="vertical")
 
-def autolabel(rects):
-    # attach some text labels
     for rect in rects:
         height = rect.get_height()
         ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
                 '%.1f' % height,
                 ha='center', va='bottom')
-
-autolabel(rects1)
-plt.show()
+    plt.show()
+rate_2011()
