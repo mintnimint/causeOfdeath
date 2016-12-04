e354b92859fa4ab304f005f2d18cffb93f5a3504
SukanlayaL <s6070140@kmitl.ac.th>
2016-12-04 11:59:40 +0700

    Add pie chart function

--- Main.py
+++ Main.py
@@ -5,8 +5,9 @@ import xlrd
 
 
 def main():
+##    pie_chart()
     """Main Programe if user input number from
-    select year ecch 2009, 2010, 2011, 2012
+    select year ecch 2009, 2010, 2011, 2012, 2013
     """
     select = int(input("Please input year (2009 - 2013): "))
     if select == 2009:
@@ -30,7 +31,7 @@ def year_2009():
     data_set = [[worksheet.cell_value(row,col) for col in range(worksheet.ncols)] for
                 row in range(worksheet.nrows)]
     n_groups = 10
-    means_men = (float(data_set[3][2]), float(data_set[4][2]), float(data_set[5][2]),\
+    data = (float(data_set[3][2]), float(data_set[4][2]), float(data_set[5][2]),\
                  float(data_set[6][2]), float(data_set[7][2]),float(data_set[8][2]),\
                  float(data_set[9][2]), float(data_set[10][2]),\
                  float(data_set[11][2]), float(data_set[12][2]))
@@ -40,7 +41,7 @@ def year_2009():
     bar_width = 0.35
     opacity = 0.4
     error_config = {'ecolor': '0.3'}
-    rects1 = plt.bar(index, means_men, bar_width,\
+    rects1 = plt.bar(index, data, bar_width,\
                      alpha=opacity,
                      color='#3300FF',
                      error_kw=error_config,
@@ -64,7 +65,7 @@ def year_2010():
     data_set = [[worksheet.cell_value(row,col) for col in range(worksheet.ncols)] for
                 row in range(worksheet.nrows)]
     n_groups = 10
-    means_men = (float(data_set[3][4]), float(data_set[4][4]), float(data_set[5][4]),\
+    data = (float(data_set[3][4]), float(data_set[4][4]), float(data_set[5][4]),\
                  float(data_set[6][4]), float(data_set[7][4]),float(data_set[8][4]),\
                  float(data_set[9][4]), float(data_set[10][4]),\
                  float(data_set[11][4]), float(data_set[12][4]))
@@ -74,7 +75,7 @@ def year_2010():
     bar_width = 0.35
     opacity = 0.4
     error_config = {'ecolor': '0.3'}
-    rects1 = plt.bar(index, means_men, bar_width,\
+    rects1 = plt.bar(index, data, bar_width,\
                      alpha=opacity,
                      color='#FF9966',
                      error_kw=error_config,
@@ -98,7 +99,7 @@ def year_2011():
     data_set = [[worksheet.cell_value(row,col) for col in range(worksheet.ncols)] for
                 row in range(worksheet.nrows)]
     n_groups = 10
-    means_men = (float(data_set[3][6]), float(data_set[4][6]), float(data_set[5][6]),\
+    data = (float(data_set[3][6]), float(data_set[4][6]), float(data_set[5][6]),\
                  float(data_set[6][6]), float(data_set[7][6]),float(data_set[8][6]),\
                  float(data_set[9][6]), float(data_set[10][6]),\
                  float(data_set[11][6]), float(data_set[12][6]))
@@ -108,7 +109,7 @@ def year_2011():
     bar_width = 0.35
     opacity = 0.4
     error_config = {'ecolor': '0.3'}
-    rects1 = plt.bar(index, means_men, bar_width,\
+    rects1 = plt.bar(index, data, bar_width,\
                      alpha=opacity,
                      color='#FF9900',
                      error_kw=error_config,
@@ -132,7 +133,7 @@ def year_2012():
     data_set = [[worksheet.cell_value(row,col) for col in range(worksheet.ncols)] for
                 row in range(worksheet.nrows)]
     n_groups = 10
-    means_men = (float(data_set[3][8]), float(data_set[4][8]), float(data_set[5][8]),\
+    data = (float(data_set[3][8]), float(data_set[4][8]), float(data_set[5][8]),\
                  float(data_set[6][8]), float(data_set[7][8]),float(data_set[8][8]),\
                  float(data_set[9][8]), float(data_set[10][8]),\
                  float(data_set[11][8]), float(data_set[12][8]))
@@ -142,7 +143,7 @@ def year_2012():
     bar_width = 0.35
     opacity = 0.4
     error_config = {'ecolor': '0.3'}
-    rects1 = plt.bar(index, means_men, bar_width,\
+    rects1 = plt.bar(index, data, bar_width,\
                      alpha=opacity,
                      color='#66CC00',
                      error_kw=error_config,
@@ -166,7 +167,7 @@ def year_2013():
     data_set = [[worksheet.cell_value(row,col) for col in range(worksheet.ncols)] for
                 row in range(worksheet.nrows)]
     n_groups = 10
-    means_men = (float(data_set[3][10]), float(data_set[4][10]), float(data_set[5][10]),\
+    data = (float(data_set[3][10]), float(data_set[4][10]), float(data_set[5][10]),\
                  float(data_set[6][10]), float(data_set[7][10]),float(data_set[8][10]),\
                  float(data_set[9][10]), float(data_set[10][10]),\
                  float(data_set[11][10]), float(data_set[12][10]))
@@ -176,7 +177,7 @@ def year_2013():
     bar_width = 0.35
     opacity = 0.4
     error_config = {'ecolor': '0.3'}
-    rects1 = plt.bar(index, means_men, bar_width,\
+    rects1 = plt.bar(index, data, bar_width,\
                      alpha=opacity,
                      color='#3366CC',
                      error_kw=error_config,
@@ -192,4 +193,48 @@ def year_2013():
     plt.tight_layout() 
     plt.show()
 
+
+def pie_chart():
+    """show graph that Plot total of cause death year 2009 - 2013"""
+    path = 'C:\Python34\ProjectPython\data1.xls'
+    workbook = xlrd.open_workbook(path) 
+    worksheet = workbook.sheet_by_index(0) 
+    data_set = [[worksheet.cell_value(row,col) for col in range(worksheet.ncols)] for
+                row in range(worksheet.nrows)]
+    
+    cancer = sum((float(data_set[3][2]), float(data_set[3][4]), float(data_set[3][6]),\
+                 float(data_set[3][8]),float(data_set[3][10])))
+    accident = sum((float(data_set[4][2]), float(data_set[4][4]), float(data_set[4][6]),\
+                 float(data_set[4][8]),float(data_set[4][10])))
+    high_bd = sum((float(data_set[5][2]), float(data_set[5][4]), float(data_set[5][6]),\
+                 float(data_set[5][8]),float(data_set[5][10])))
+    heart_ds = sum((float(data_set[6][2]), float(data_set[6][4]), float(data_set[6][6]),\
+                 float(data_set[6][8]),float(data_set[6][10])))
+    lung_ds = sum((float(data_set[7][2]), float(data_set[7][4]), float(data_set[7][6]),\
+                 float(data_set[7][8]),float(data_set[7][10])))
+    nephrit = sum((float(data_set[8][2]), float(data_set[8][4]), float(data_set[8][6]),\
+                 float(data_set[8][8]),float(data_set[8][10])))
+    liver_ds = sum((float(data_set[9][2]), float(data_set[9][4]), float(data_set[9][6]),\
+                 float(data_set[9][8]),float(data_set[9][10])))
+    commit_sc = sum((float(data_set[10][2]), float(data_set[10][4]), float(data_set[10][6]),\
+                 float(data_set[10][8]),float(data_set[10][10])))
+    diabetes = sum((float(data_set[11][2]), float(data_set[11][4]), float(data_set[11][6]),\
+                 float(data_set[11][8]),float(data_set[11][10])))
+    tubercul = sum((float(data_set[12][2]), float(data_set[12][4]), float(data_set[12][6]),\
+                 float(data_set[12][8]),float(data_set[12][10])))
+    data = (float(cancer), float(accident), float(high_bd),float(heart_ds), float(lung_ds),\
+            float(), float(liver_ds), float(commit_sc),float(diabetes), float(tubercul))
+
+    labels = "Cancer and Tumor", "Accidents", "High Blood Pressure", "Heart Disease",\
+             "Lung Disease","Nephritis", "Liver Disease", "Commit Suicide", "Diabetes",\
+             "Tuberculosis"
+    colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'lightgrey',\
+              'tomato', 'seagreen', 'royalblue', 'sienna', 'tan']
+    explode = (0.1, 0, 0, 0,0,0.1, 0, 0, 0,0) 
+    plt.pie(data, explode=explode, labels=labels, colors=colors,\
+            autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.axis('equal')
+    plt.legend()
+    plt.show()
+    
 main()
